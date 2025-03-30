2845e94906942d41b9d5dc4b941735efe924769c
sdas10-26 <dwishitd@gmai.com>
2025-03-30 15:01:13 -0400

    adding

--- git_exercise.py
+++ git_exercise.py
@@ -1,5 +1,12 @@
+
+
 def say_hello():
     print("Hello")
-
 if __name__ == "__main__":
     say_hello()
+
+
+def say_goodbye():
+    print("goodbye")
+if __name__ == "__main__":
+    say_goodbye()            
